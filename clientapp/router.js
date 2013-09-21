93a4918635f4959d390b7476a821cdfdd367c992
Isaac Lewis <isaac@andyet.net>
2013-09-21 12:11:02 -0700

    remove some default pages

--- clientapp/router.js
+++ clientapp/router.js
@@ -1,33 +1,16 @@
 /*global app, me, $*/
 var Backbone = require('backbone');
 var HomePage = require('./pages/home');
-var CollectionDemo = require('./pages/collectionDemo');
-var InfoPage = require('./pages/info');
 
 
 module.exports = Backbone.Router.extend({
     routes: {
-        '': 'home',
-        'collections': 'collectionDemo',
-        'info': 'info'
+        '': 'home'
     },
 
     // ------- ROUTE HANDLERS ---------
     home: function () {
         app.renderPage(new HomePage({
-            model: me
-        }));
-    },
-
-    collectionDemo: function () {
-        app.renderPage(new CollectionDemo({
-            model: me,
-            collection: app.people
-        }));
-    },
-
-    info: function () {
-        app.renderPage(new InfoPage({
             model: me
         }));
     }
